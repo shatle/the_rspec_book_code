a8400af49eb3ff6429804ca22a6f9660ec79d368
shatle <shatle@qq.com>
2013-04-09 20:09:22 +0800

    update to 4.4

--- cb/04/features/step_definitions/codebreaker_steps.rb
+++ cb/04/features/step_definitions/codebreaker_steps.rb
@@ -4,3 +4,8 @@ end
 When /^I start a new game$/ do 
 	Codebreaker::Game.new.start
 end 
+
+Then /^I should see "(.*?)"$/ do |message|
+  #pending # express the regexp above with the code you wish you had
+	output.messages.should include(message)
+end
